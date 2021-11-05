9bc3438a9498f07f7a1e60f127fa37c5705213cd
Zachary Huang <zack466@gmail.com>
2021-11-05 18:21:51 -0400

    day 19 start

---
+++ 2020/day19.py
@@ -0,0 +1,13 @@
+class Rule:
+    pass
+
+class And:
+    def __init__()
+
+def matches(rule, rules):
+
+def sol(input: str) -> int:
+    return 0
+
+with open("input_day19") as f:
+    print(sol(f.read()))
